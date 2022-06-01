2a6780b9a1457bf3ea72aecae7df90c0aaf17bc3
Rizwana71 <rizwanajamil71@gmail.com>
2022-05-31 21:37:16 -0400

    1st commit

---
+++ src/gitReview/gitReview01.java
@@ -0,0 +1,9 @@
+package gitReview;
+
+public class gitReview01 {
+	public static void main(String[] Args) {
+		System.out.print("today we learn github");
+		System.out.print("today is a very good day");
+	}
+
+}
